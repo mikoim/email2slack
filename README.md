f74823736d77358525407187d0ce7882a9105c01
Eshin Kunishima <ek@esh.ink>
2016-05-20 00:11:25 +0900

    initial code

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # email2slack
-E-mail forwarding script for Slack written in Python
+
+E-mail forwarding script for Slack written in Python.
+I tested on Python 3.x and Postfix.
+
+## Demo
+
+![Slack](slack-demo.png)
+
+## Requirements
+
+ - Python 3.x (Not tested in Python 2.x)
+ - chardet : https://github.com/chardet/chardet
+ - requests : https://github.com/kennethreitz/requests
+
+## Getting Started
+
+```bash
+git clone https://github.com/mikoim/email2slack.git
+cd email2slack
+
+# Install dependencies
+pip3 install -r requirements.txt
+
+# Install email2slack
+cp email2slack.py /usr/local/bin/email2slack.py && chmod +x /usr/local/bin/email2slack.py
+cp email2slack /usr/local/etc/
+
+# Before using, You must edit config file
+vim /usr/local/etc/email2slack
+```
+
+### Postfix
+```bash
+vim /etc/postfix/aliases
+
+...
+
+iamauser: |/usr/local/bin/email2slack.py
+
+...
+
+newaliases
+```
---
+++ email2slack.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+import sys
+from configparser import ConfigParser
+from email.parser import Parser
+
+import chardet
+import re
+import requests
+
+
+class EmailParser:
+    @staticmethod
+    def parse(mime_mail):
+        parsed_mail = Parser().parsestr(mime_mail)
+        result = {
+            'From': parsed_mail['From'],
+            'To': parsed_mail['To'],
+            'Subject': parsed_mail['Subject'],
+            'body-plain': None,
+            'body-html': None
+        }
+
+        messages = []
+        if parsed_mail.is_multipart():
+            for m in parsed_mail.get_payload():
+                messages.append(EmailParser.extract_message(m))
+        else:
+            messages.append(EmailParser.extract_message(parsed_mail))
+
+        for m in messages:
+            content_type = m[0]
+            body = m[1]
+
+            if content_type.startswith('text/plain'):
+                result['body-plain'] = body
+            elif content_type.startswith('text/html'):
+                result['body-html'] = body
+
+        return result
+
+    @staticmethod
+    def extract_message(message):
+        body = message.get_payload(decode=True)
+        return message['Content-Type'], body.decode(encoding=chardet.detect(body)['encoding'])
+
+
+class Slack:
+    def __init__(self):
+        cfg = ConfigParser()
+        cfg.read(['email2slack', '~/.email2slack', '/etc/email2slack', '/usr/local/etc/email2slack'])
+
+        slack = {s[0]: s[1] for s in cfg.items('Slack')}
+        self.__team = [(re.compile(t[0]), slack[t[1]]) for t in cfg.items('Team')]
+        self.__channel = [(re.compile(c[0]), c[1]) for c in cfg.items('Channel')]
+
+    def notice(self, mail):
+        address_to = mail['To']
+        address_from = mail['From']
+        subject = mail['Subject']
+        body = mail['body-plain']
+
+        text = 'From: {:s}\nTo: {:s}\nSubject: {:s}\n\n{:s}'.format(address_from, address_to, subject, body)
+
+        url = [r[1] for r in self.__team if r[0].match(address_to)]
+        if url is None:
+            raise Exception('team not found: {:s}'.format(address_to))
+
+        channel = [r[1] for r in self.__channel if r[0].match(address_to)]
+        if channel is None:
+            raise Exception('channel not found: {:s}'.format(address_to))
+
+        self.__post(url[0], self.__payload(text, channel=channel[0]))
+
+    @staticmethod
+    def __payload(text, username=None, channel=None):
+        result = {'text': text}
+
+        if username:
+            result['username'] = username
+        if channel:
+            result['channel'] = channel
+
+        return result
+
+    @staticmethod
+    def __post(url, body):
+        requests.post(url, json=body)
+
+
+if __name__ == '__main__':
+    raw_mail = ''.join([x for x in sys.stdin.read() if x is not None])
+    mail = EmailParser.parse(raw_mail)
+    Slack().notice(mail)
