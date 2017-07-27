22de74ea7317f87cd2545681b24ef0adeaa87467
Eshin Kunishima <ek@esh.ink>
2017-07-28 00:20:53 +0900

    bump version to v1.0.0-alpha.4

--- setup.py
+++ setup.py
@@ -9,29 +9,21 @@ except:
 
 setup(
     name='email2slack',
-
-    version='1.0.0a3',
-
+    version='1.0.0a4',
     description='MIME E-mail forwarding script for Slack written in Python',
     long_description=long_description,
-
     url='https://github.com/mikoim/email2slack',
-
     author='Eshin Kunishima',
     author_email='ek@esh.ink',
-
+    packages=['email2slack'],
     license='MIT',
-
     classifiers=[
         'Development Status :: 3 - Alpha',
-
         'Topic :: Communications',
         'Topic :: Communications :: Email',
         'Topic :: Communications :: Email :: Mail Transport Agents',
         'Topic :: Communications :: Chat',
-
         'License :: OSI Approved :: MIT License',
-
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
@@ -41,11 +33,16 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
-
     keywords='email slack forwarding',
-
-    py_modules=['email2slack'],
-
+    requires=[
+        'beautifulsoup4',
+        'certifi',
+        'chardet',
+        'idna',
+        'lxml',
+        'requests',
+        'urllib3',
+    ],
     install_requires=[
         'beautifulsoup4==4.6.0',
         'certifi==2017.4.17',
@@ -55,7 +52,6 @@ setup(
         'requests==2.18.2',
         'urllib3==1.22',
     ],
-
     entry_points={
         'console_scripts': [
             'email2slack = email2slack:main',
